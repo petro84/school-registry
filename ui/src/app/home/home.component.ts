44ac373751432855ddf20fb4cbab21a62b30daf9
Petro <larrypetroski@gmail.com>
2022-11-24 11:54:11 -0600

    Partially implemented the home page component.  Student button needs updating.

--- ui/src/app/home/home.component.ts
+++ ui/src/app/home/home.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { faBookReader } from '@fortawesome/free-solid-svg-icons';
+
+import { Grade } from '../models/grade.model';
+import { GradesService } from '../services/grades.service';
+
 @Component({
   selector: 'sr-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  grades!: Grade[];
 
-  constructor() { }
+  faBookReader = faBookReader;
+
+  constructor(private gradesSvc: GradesService) {}
 
   ngOnInit(): void {
+    this.gradesSvc.grades().subscribe((grades) => (this.grades = grades));
   }
-
 }
